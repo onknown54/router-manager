0097623aff0e99ca7181b9f098de904f4dd5bda9
onknown54 <lekanm715@gmail.com>
2024-03-04 00:38:13 +0100

    starter files

---
+++ main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 500,
+  });
+
+  win.loadFile("./renderer/index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
