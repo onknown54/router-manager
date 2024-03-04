3812ff7ea5965c756689be2d3c7a6924b40189c1
onknown54 <lekanm715@gmail.com>
2024-03-04 11:48:59 +0100

    fixes

--- main.js
+++ main.js
@@ -2,20 +2,29 @@ process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
-const createMainWindow = () => {
+function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 800,
+    width: 750,
     height: 500,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
-      enableRemoteModule: true,
     },
   });
 
-  mainWindow.loadFile(path.join(__dirname, "renderer/index.html"));
-};
+  // Use loadFile method to load the HTML file
+  const filePath = path.join(__dirname, "./src/renderer/index.html");
+  mainWindow.loadURL(`file://${filePath}`);
+}
 
 app.whenReady().then(() => {
-  createMainWindow();
+  createWindow();
+
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
 });
