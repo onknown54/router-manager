04dad73a6342d1d0395ac6315d651cc03d958ea7
onknown54 <lekanm715@gmail.com>
2024-03-05 12:55:34 +0100

    fixes issue with context bridge

--- main.js
+++ main.js
@@ -1,41 +1,28 @@
 // process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
 const { app, BrowserWindow, ipcMain } = require("electron/main");
-const path = require("node:path");
-
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
-  });
-  win.loadFile("index.html");
-};
-app.whenReady().then(() => {
-  ipcMain.handle("ping", () => "pong");
-  createWindow();
-});
 const path = require("path");
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 750,
     height: 500,
+    minWidth: 750,
+    minHeight: 500,
+    maxWidth: 750,
+    maxHeight: 500,
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
+      nodeIntegration: false,
+      contextIsolation: true,
       preload: path.join(__dirname, "./preload.js"),
     },
   });
 
   // Use loadFile method to load the HTML file
-  const filePath = path.join(__dirname, "./src/renderer/index.html");
-  mainWindow.loadURL(`file://${filePath}`);
+  mainWindow.loadURL(path.join(__dirname, "./src/renderer/index.html"));
 }
 
 app.whenReady().then(() => {
-  ipcMain.handle("ping", () => "pong");
+  ipcMain.handle("ping", () => "yes my man, pink to the puke to the pong!!!");
   createWindow();
 
   app.on("activate", function () {
