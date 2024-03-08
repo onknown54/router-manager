e3907f7f8185c1a811d3bda50c1ccbf52dfad018
onknown54 <lekanm715@gmail.com>
2024-03-08 03:41:09 +0100

    worked on app naviagation

--- src/renderer/js/dashboard.js
+++ src/renderer/js/dashboard.js
@@ -1,6 +1,17 @@
-[...document.querySelectorAll(".dashboard .navigation__link")].forEach((el) => {
+const navLinks = document.querySelectorAll(".dashboard .navigation__link");
+
+navLinks.forEach((el) => {
   el.addEventListener("click", function (e) {
     e.preventDefault();
-    versions.loadNestPage("load-next-page", this.textContent.toLowerCase());
+    navLinks.forEach((el) => el.classList.remove("active"));
+    this.classList.add("active");
+
+    let page = this.textContent.split(" ").map((itm, idx) => {
+      return idx
+        ? itm.substr(0, 1).toUpperCase() + itm.substr(1)
+        : itm.toLowerCase();
+    });
+
+    versions.loadNestPage("load-next-page", page.join(""));
   });
 });
