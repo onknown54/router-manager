0ab5282acc5ab4511dc4ebca36771a0c0b389b56
onknown54 <lekanm715@gmail.com>
2024-03-16 03:10:06 +0100

    refactored codebase

--- src/renderer/js/profile.js
+++ src/renderer/js/profile.js
@@ -1,23 +1,26 @@
 "use strict";
 
-(async () => {
-  const [{ firstname, lastname, username, email, phone }] =
-    await versions.getData();
-  const profile = document.querySelector(
-    ".profile__details .table.table-proInfo"
-  );
-  const renderHtmlMarkup = (obj, markupBody) => {
-    for (let key in obj) {
-      markupBody.innerHTML =
-        markupBody.innerHTML +
-        `<tr>
+const renderHtmlMarkup = (obj, markupBody) => {
+  for (let key in obj) {
+    markupBody.innerHTML =
+      markupBody.innerHTML +
+      `<tr>
           <th>${key}</th>
           <td>${obj[key]}</td>
         </tr>`;
-    }
+  }
+};
 
-    return;
-  };
-
-  renderHtmlMarkup({ firstname, lastname, username, email, phone }, profile);
-})();
+(async () =>
+  await versions
+    .getData()
+    .then(([{ firstname, lastname, username, email, phone }]) => {
+      const profile = document.querySelector(
+        ".profile__details .table.table-proInfo"
+      );
+      renderHtmlMarkup(
+        { firstname, lastname, username, email, phone },
+        profile
+      );
+    })
+    .catch(console.error))();
